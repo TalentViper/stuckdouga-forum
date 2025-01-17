51816b8b8aa9f3e217f064d8c8dfbbffdb3bec1d
TalentViper <steven.infinity2025@gmail.com>
2025-01-17 07:09:42 -0500

    fix: email verification contents

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ static_asset('images/img/stuckdouga_logo.jpg') }}" class="logo" alt="stuckdouga Logo">
+
+</a>
+</td>
+</tr>
